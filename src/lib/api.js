16f8c7ac965e830b51b19fed81d8dc5e28a2f25b
Karlito16 <karlo.dimjasevic@gmail.com>
2025-06-11 11:35:18 +0200

    Side menu, buildings table and view building pages; API layer

---
+++ src/lib/api.js
@@ -0,0 +1,35 @@
+// TODO: Use TypeScript
+
+const ASSETS_SERVICE_API = "http://localhost:8080/api";
+
+export async function getBuildings(page, size) {
+	const res = await fetch(`${ASSETS_SERVICE_API}/v1/buildings?page=${page}&size=${size}`);
+
+	if (!res.ok) throw new Error("Error fetching the data");
+
+	return await res.json();
+}
+
+export async function getBuildingFloor(buildingId, floorId) {
+	const res = await fetch(`${ASSETS_SERVICE_API}/v1/buildings/${buildingId}/floors/${floorId}`);
+
+	if (!res.ok) throw new Error("Error fetching the data");
+
+	return await res.json();
+}
+
+export async function getBuildingFloorPresence(buildingId, floorId) {
+	const res = await fetch(`${ASSETS_SERVICE_API}/v1/buildings/${buildingId}/floors/${floorId}/users`);
+
+	if (!res.ok) throw new Error("Error fetching the data");
+
+	return await res.json();
+}
+
+export async function getBuildingFloorUserPresence(buildingId, floorId, userId) {
+	const res = await fetch(`${ASSETS_SERVICE_API}/v1/buildings/${buildingId}/floors/${floorId}/users/${userId}`);
+
+	if (!res.ok) throw new Error("Error fetching the data");
+
+	return await res.json();
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,113 +1,13 @@
 <script>
-	import { onMount } from "svelte";
 
-	let data;
-	let cellGrid;
-
-	onMount(async () => {
-		const module = await import("/src/db/fer_building.js")
-		data = module.data;
-
-		const floor = data.floors[0];
-		const rooms = floor.rooms;
-
-		// Calculate a number of rows and cols
-		let rows = 0;
-		let cols = 0;
-		rooms.forEach(({ roomVisualization: rv }) => {
-			rows = Math.max(rows, rv.startRow + rv.rowspan);
-			cols = Math.max(cols, rv.startCol + rv.colspan);
-		});
-
-		// Init table grid
-		cellGrid = Array.from(
-			{ length: rows },
-			() => Array.from(
-				{ length: cols },
-				() => (
-					{ type: "hall", render: true, rowspan: 1, colspan: 1, room: null }	// Initially, all cells are hall
-				)
-			)
-		);
-
-		// Place rooms and mark merged cells
-		for (const room of rooms) {
-			const { startRow, startCol, rowspan, colspan } = room.roomVisualization;
-			cellGrid[startRow][startCol] = {
-				type: "room",	// CSS class
-				render: true, // Will render that cell (html td tag)
-				rowspan,
-				colspan,
-				room
-			};
-
-			for (let r = startRow; r < startRow + rowspan; r++) {
-				for (let c = startCol; c < startCol + colspan; c++) {
-					if (r === startRow && c === startCol) continue;
-					cellGrid[r][c].render = false;	// Won't render since this cell (html td tag) will eventually be merged
-				}
-			}
-		}
-	})
 </script>
 
-<div class="container">
-	{#if data}
-		<h1><b>{data.name}</b></h1>
-		<table>
-			<tbody>
-			{#each cellGrid as row, rowIndex (`row-{${rowIndex}}`)}
-				<tr>
-					{#each row as cell, cellIndex (`cell-${cellIndex}`)}
-						{#if cell.render}
-							<td
-								class={cell.type}
-								rowspan={cell.rowspan}
-								colspan={cell.colspan}
-							>
-								{cell.room?.name}
-							</td>
-						{/if}
-					{/each}
-				</tr>
-			{/each}
-			</tbody>
-		</table>
-	{:else}
-		<p>Loading mock data...</p>
-	{/if}
-</div>
+<h1 class="page-title">Home</h1>
 
 <style>
-    table {
-        border-collapse: collapse;
-        border: 2px solid black;
-    }
-
-    td {
-        width: 50px;
-        height: 50px;
-        border: 1px solid #999;
-        text-align: center;
-        vertical-align: middle;
-        position: relative;
-    }
-
-		.container {
-				display: flex;
-				flex-direction: column;
-				justify-content: start;
-				align-items: center;
-				width: 100vw;
-				height: 100vh;
-		}
-
-    .room {
-        background-color: #cce5ff;
-    }
-
-    .hall {
-        background-color: #e2e3e5;
-        border: none;
+    .page-title {
+        font-size: 1.8rem;
+        margin-bottom: 1rem;
+        color: #333;
     }
 </style>
